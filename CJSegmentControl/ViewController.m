53e658aafa72fc49cadc2fef9a75a58ad3c2a279
jing.cai <jing.cai1@baidao.com>
2017-02-17 09:21:21 +0800

    update to 1.1.1

--- CJSegmentControl/ViewController.m
+++ CJSegmentControl/ViewController.m
@@ -18,28 +18,34 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    UIScrollView *scr = [[UIScrollView alloc] init];
-    scr.delegate = self;
-    scr.pagingEnabled = YES;
-    _scr = scr;
-    CJSegmentControl *segementControl = [[CJSegmentControl alloc] initWithFrame:CGRectMake(0, 64, self.view.frame.size.width, 30) titles:@[@"123",@"AAA",@"VVV",@"CCC",@"123",@"AAA",@"VVV",@"CCC"] selectionWidth:80 scr:_scr];
+
+    CJSegmentControl *segementControl = [[CJSegmentControl alloc] initWithFrame:CGRectMake(0, 64, self.view.frame.size.width, 30) titles:@[@"123",@"AAA",@"VVV",@"CCC",@"123",@"AAA",@"VVV",@"CCC"] selectionWidth:80 scr:self.scr];
     segementControl.backgroundColor = [UIColor whiteColor];
     segementControl.gradientOffset = -50;
     segementControl.gradientBottomMargin = 5;
     
-    scr.frame = CGRectMake(0, CGRectGetMaxY(segementControl.frame), self.view.frame.size.width, self.view.frame.size.height - CGRectGetMaxY(segementControl.frame));
-    int titleCount = 8;
-    scr.contentSize = CGSizeMake(scr.frame.size.width * titleCount, scr.frame.size.height);
-    scr.pagingEnabled = YES;
-    for (int i = 0; i < titleCount; i++) {
-        UIView *view = [[UIView alloc] initWithFrame:CGRectMake(i * scr.frame.size.width, 0, scr.frame.size.width, scr.frame.size.height)];
-        CGFloat colorRatio = (i + 1) / (CGFloat)titleCount;
-        view.backgroundColor = [UIColor colorWithRed:colorRatio green:colorRatio blue:colorRatio alpha:01];
-        [scr addSubview:view];
-    }
-    [self.view  addSubview:scr];
     [self.view  addSubview:segementControl];
     // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (UIScrollView *)scr{
+    if (!_scr) {
+        UIScrollView *scr = [[UIScrollView alloc] init];
+        scr.pagingEnabled = YES;
+        scr.frame = CGRectMake(0, 94, self.view.frame.size.width, self.view.frame.size.height - 94);
+        int titleCount = 8;
+        scr.contentSize = CGSizeMake(scr.frame.size.width * titleCount, scr.frame.size.height);
+        scr.pagingEnabled = YES;
+        for (int i = 0; i < titleCount; i++) {
+            UIView *view = [[UIView alloc] initWithFrame:CGRectMake(i * scr.frame.size.width, 0, scr.frame.size.width, scr.frame.size.height)];
+            CGFloat colorRatio = (i + 1) / (CGFloat)titleCount;
+            view.backgroundColor = [UIColor colorWithRed:colorRatio green:colorRatio blue:colorRatio alpha:01];
+            [scr addSubview:view];
+        }
+        [self.view  addSubview:scr];
+        _scr = scr;
+    }
+    return _scr;
 }
 
 @end
